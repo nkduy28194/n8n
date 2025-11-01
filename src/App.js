3c366862e98e003c9137e698b82e391f8fd56db9
nkduy28194 <nkduy28194@gmail.com>
2025-11-01 10:17:33 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React 2025
         </a>
       </header>
     </div>
